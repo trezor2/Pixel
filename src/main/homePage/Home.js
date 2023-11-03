fd1b2e69b59d4e7d9ea7a2ad0b27a1c9e46243ba
trezor1 <chriskwon0@gmail.com>
2023-11-03 16:11:40 +1300

    Feat: Website character movement implementation
    This commit adds the basic functionality of the movement for the character which may be used by the user to select options.

--- src/main/homePage/Home.js
+++ src/main/homePage/Home.js
@@ -1,27 +1,62 @@
-// JavaScript code to fetch cryptocurrency data from an API
-const apiUrl = 'https://api.example.com/crypto-prices'; // Replace this with the actual API endpoint URL
+const character = document.getElementById('character');
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+const optionWidth = character.clientWidth; // Assuming option1 and option2 have the same width
+const optionHeight = character.clientHeight; // Assuming option1 and option2 have the same height
+const maxLeft = window.innerWidth - character.clientWidth - optionWidth; // Calculate the maximum left position dynamically
+const maxTop = window.innerHeight - character.clientHeight - optionHeight; // Calculate the maximum top position dynamically
 
-// Fetch cryptocurrency data from the API
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const cryptoListContainer = document.getElementById('crypto-list');
+document.addEventListener('keydown', function(event) {
+    const characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
 
-        // Iterate through the cryptocurrency data and create HTML elements for each crypto
-        data.forEach(crypto => {
-            const cryptoCard = document.createElement('div');
-            cryptoCard.className = 'crypto-card';
-            cryptoCard.innerHTML = `
-                <h2>${crypto.name}</h2>
-                <p>Symbol: ${crypto.symbol}</p>
-                <p>Price: $${crypto.price}</p>
-                <!-- Add more data properties as needed -->
-            `;
+    // Check for left arrow key press
+    if (event.key === 'ArrowLeft' && characterLeft > 0) {
+        character.style.left = (characterLeft - 50) + 'px';
+    }
 
-            // Append the crypto card to the crypto list container
-            cryptoListContainer.appendChild(cryptoCard);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching crypto data:', error);
-    });
+    // Check for right arrow key press
+    if (event.key === 'ArrowRight' && characterLeft < maxLeft) {
+        character.style.left = (characterLeft + 50) + 'px';
+    }
+
+    // Check for up arrow key press
+    if (event.key === 'ArrowUp' && characterTop > 0) {
+        character.style.top = (characterTop - 50) + 'px';
+    }
+
+    // Check for down arrow key press
+    if (event.key === 'ArrowDown' && characterTop < maxTop) {
+        character.style.top = (characterTop + 50) + 'px';
+    }
+
+    // Check if the character overlaps with options
+    const characterRect = character.getBoundingClientRect();
+    const option1Rect = option1.getBoundingClientRect();
+    const option2Rect = option2.getBoundingClientRect();
+
+    if (isOverlap(characterRect, option1Rect)) {
+        highlightOption(option1, option2);
+    } else if (isOverlap(characterRect, option2Rect)) {
+        highlightOption(option2, option1);
+    } else {
+        resetOptionsColor(option1, option2);
+    }
+});
+
+function isOverlap(rect1, rect2) {
+    return !(rect1.right < rect2.left || 
+             rect1.left > rect2.right || 
+             rect1.bottom < rect2.top || 
+             rect1.top > rect2.bottom);
+}
+
+function highlightOption(selectedOption, unselectedOption) {
+    selectedOption.style.backgroundColor = 'yellow';
+    unselectedOption.style.backgroundColor = '#4CAF50';
+}
+
+function resetOptionsColor(option1, option2) {
+    option1.style.backgroundColor = '#4CAF50';
+    option2.style.backgroundColor = '#4CAF50';
+}
